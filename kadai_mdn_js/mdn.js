d32f975fcd016cdf96ffbb287cd9c2054ef4d1db
556dream <taimu.1126@icloud.com>
2023-11-21 15:46:11 +0900

    課題提出

--- kadai_mdn_js/mdn.js
+++ kadai_mdn_js/mdn.js
@@ -1,6 +1,6 @@
-const birthday = new Date(2023, 8, 22);
+const birthday = new Date();
 const day = birthday.getDate();
 const year = birthday.getFullYear();
-const month = birthday.getMonth();
+const month = birthday.getMonth()+1;
 
 console.log(year+ "年" + month + "月" + day + "日");
